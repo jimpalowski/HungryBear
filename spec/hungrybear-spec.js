dbe8467a386ef307205370900ce515d33f2f2a91
CJ Rhone and Jim Palowski <cj@>
2018-05-29 10:20:41 -0700

    Initial Commit

---
+++ spec/hungrybear-spec.js
@@ -0,0 +1,23 @@
+import { HungryBear } from '../src/js/hungrybear.js';
+
+describe('HungryBear', function(){
+  let fuzzy = new HungryBear("Fuzzy");
+
+  beforeEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  it('should have a name and a food level of 10 when it is created', function(){
+    expect(fuzzy.name).toEqual("Fuzzy");
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
+
+  it('should have a food level of 7 after 3001 milliseconds', function(){
+    jasmine.clock(3001);
+    expect(fuzzy.foodLevel).toEqual(7);
+  });
+});
